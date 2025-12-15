3c63484531f3812d6d7ee67035d7d834d788a4f4
divyanshu9166 <technods02@gmail.com>
2025-12-15 18:25:29 +0530

    Update logo image and remove testimonials section

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
 import { Projects } from './components/Projects';
 import { Amenities } from './components/Amenities';
-import { Testimonials } from './components/Testimonials';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
@@ -16,7 +15,6 @@ function App() {
         <Hero />
         <Projects />
         <Amenities />
-        <Testimonials />
         <Contact />
       </main>
 
